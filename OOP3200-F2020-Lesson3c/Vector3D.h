6b7d8d9cdb3b3a4636f64ce7f8b66b7842793552
kaif vakil <kaifvakil02@gmail.com>
2020-10-14 12:37:50 -0400

    Added main class functioanlity

--- OOP3200-F2020-Lesson3c/Vector3D.h
+++ OOP3200-F2020-Lesson3c/Vector3D.h
@@ -3,12 +3,17 @@
 #define _VECTOR3D_
 
 #include <string>
+#include <iostream>
+#include "Vector2D.h"
+#include <assert.h>
 
 template <class T>
 class Vector3D
 {
 public:
-	Vector3D(T x = static_cast<T>(0.0f), T y = static_cast<T>(0.0f), T z = static_cast<T>(0.0f));
+	Vector3D(T x = static_cast<T>(0.0), T y = static_cast<T>(0.0), T z = static_cast<T>(0.0));
+	Vector3D(const std::string& x, const std::string& y, std::string& z);
+	Vector3D(const Vector2D& vector_2d);
 	~Vector3D();
 
 	//Setters
@@ -21,8 +26,26 @@ public:
 	T GetX() const ;
 	T GetY() const ;
 	T GetZ() const ;
+
+	// input/output operator overloads
+	friend std::ostream& operator<<(std::ostream& out, const Vector3D& rhs)
+	{
+		out << rhs.ToString();
+		return out;
+	}
 	
-	std::string ToString();
+	friend std::istream& operator>>(std::istream& in, Vector3D& rhs)
+	{
+		in >> rhs.m_x;
+		in.ignore();
+		in >> rhs.m_y;
+		in.ignore();
+		in >> rhs.m_z;
+
+		return in;
+	}
+	
+	std::string ToString() const ;
 	
 private:
 	T m_x;
@@ -31,6 +54,103 @@ private:
 	
 	
 };
+
+/* Implementation*/
+
+template <class T>
+Vector3D<T>::Vector3D(const T x, const T y, const  T z) : m_x(x), m_y(y), m_z(z)
+{
+}
+
+template <class T>
+Vector3D<T>::Vector3D(const std::string& x, const std::string& y, std::string& z)
+{
+	if(typeid(T) == typeid(int))
+	{
+		m_x = std::stoi(x);
+		m_y = std::stoi(y);
+		m_z = std::stoi(z);
+	}
+
+	if(typeid(T) == typeid(float))
+	{
+		m_x = std::stof(x);
+		m_y = std::stof(y);
+		m_z = std::stof(z);
+	}
+
+	if (typeid(T) == typeid(double))
+	{
+		m_x = std::stod(x);
+		m_y = std::stod(y);
+		m_z = std::stod(z);
+	}
+
+}
+
+
+template <class T>
+Vector3D<T>::Vector3D(const Vector2D& vector_2d)
+{
+	m_x = static_cast<T>(vector_2d.GetX());
+	m_y = static_cast<T>(vector_2d.GetY());
+	m_z = static_cast<T>(0.0);
+}
+
+template <class T>
+Vector3D<T>::~Vector3D()
+{}
+
+template <class T>
+void Vector3D<T>::setX(const T x)
+{
+	m_x = x;
+}
+
+template <class T>
+void Vector3D<T>::setY(const T y)
+{
+	m_y = y;
+}
+
+template <class T>
+void Vector3D<T>::setZ(const T z)
+{
+	m_z = z;
+}
+
+template <class T>
+void Vector3D<T>::set(const T x, const T y, const T z)
+{
+	m_x = x;
+	m_y = y;
+	m_z = z;
+}
+
+template <class T>
+T Vector3D<T>::GetX() const
+{
+	return m_x;
+}
+
+template <class T>
+T Vector3D<T>::GetY() const
+{
+	return m_y;
+}
+
+template <class T>
+T Vector3D<T>::GetZ() const
+{
+	return m_z;
+}
+
+template <class T>
+std::string Vector3D<T>::ToString() const 
+{
+	return "(" + std::to_string(m_x) + ", " + std::to_string(m_y) + ", " + std::to_string(m_z) + ")";
+
+}
 
 
 #endif
